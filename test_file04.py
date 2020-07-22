d9de5b80f76d9600f5e4d8f92141466778ceae0d
John Okey <john.okey@team.telstra.com>
2020-07-22 14:38:56 +1000

    adding new python files

---
+++ test_file04.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+from pprint import pprint
+
+#reg=sys.argv[1]
+
+#pprint(re.findall(r'reg[a-z]+',str(dir(os))))
+pprint(re.findall(r'get[a-z]+',str(dir(os))))
+
+print("\n\n\tHere is some modification to the previous version\n")
+print("\n\n\tHere is some modification to the previous version\n")
+print("\n\n\tHere is some modification to the previous version\n")
