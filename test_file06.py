3f7e8724f25342a1b1852f70a6926e4c339afe5c
John Okey <john.okey@team.telstra.com>
2020-07-22 15:25:23 +1000

    test 06 to be added to develop branch

---
+++ test_file06.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+from pprint import pprint
+
+#reg=sys.argv[1]
+
+#pprint(re.findall(r'reg[a-z]+',str(dir(os))))
+pprint(re.findall(r'get[a-z]+',str(dir(os))))
+
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
+print("\n\n\tblah die blah die blah blah\n")
